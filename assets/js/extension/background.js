bd2655388c85cbaf447d03af568c68c52efc96a2
Gert Glükmann <gglukmann@gmail.com>
2020-02-15 09:54:02 +0200

    Reduce permissions required

--- assets/js/extension/background.js
+++ assets/js/extension/background.js
@@ -1,5 +1,5 @@
 // Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(tab => {
+chrome.browserAction.onClicked.addListener(() => {
     // Send a message to the active tab
     chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
         const activeTab = tabs[0];
@@ -7,7 +7,7 @@ chrome.browserAction.onClicked.addListener(tab => {
     });
 });
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener(request => {
     if (request.message === 'open_new_tab') {
         chrome.tabs.create({ url: 'index.html' });
     }
--- assets/js/extension/content.js
+++ assets/js/extension/content.js
@@ -1,4 +1,4 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener(request => {
     if (request.message === 'clicked_browser_action') {
         chrome.runtime.sendMessage({ message: 'open_new_tab' });
     }
